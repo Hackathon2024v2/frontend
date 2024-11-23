e2084074b4411cd76d830a3a010ab9f1f1ce8408
GiardMathieu <mathieu.giard@hotmail.com>
2024-11-23 18:30:14 -0500

    Leaderboard finished?

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -32,7 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return userLoggedIn() == true ?
+    return userLoggedIn() == false ?
       Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
