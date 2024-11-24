95dc31410a8ead62c0a32322504699fcf342a262
p1xlized <apaduret@proton.me>
2024-11-23 19:47:04 -0500

    ADD - Working chat with gpt

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -32,7 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return userLoggedIn() == true ?
+    return userLoggedIn() == false ?
       Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -57,7 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
         [
           const Workouts(),
           const Profile(),
-          const Chat(),
+           Chat(),
           const Leaderboard(),
           Nutrition()
         ][currentPageIndex],
