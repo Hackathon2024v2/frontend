fde2509e73a50ee00ec6d30feb89faa58be5973b
samy <s_issiak@live.concordia.ca>
2024-11-24 05:45:48 -0500

    update home ui commit

--- lib/pages/workouts.dart
+++ lib/pages/workouts.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/pages/info_gym.dart';
 import 'package:flutter_application_2/pages/info_page.dart';
+import 'package:flutter_application_2/widgets/workout_type_button.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+import '../main.dart';
 import '../widgets/activity_card.dart';
 
 class Workouts extends StatefulWidget {
@@ -16,192 +18,49 @@ class _WorkoutsState extends State<Workouts> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Workouts'),
+        backgroundColor: color,
+        title: const Text(
+          'WORKOUTS',
+          style: TextStyle(
+            color: Colors.white, // Red color
+            fontStyle: FontStyle.italic, // Italic style
+            fontSize: 24, // Optional: Adjust size
+          ),
+        ),
+        elevation: 0,
+        centerTitle: true,
       ),
+      backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
-              // Row for Gym + Plus button on the right
-              Row(
-                children: [
-                  // Activity card
-                  Expanded(
-                    child: ActivityCard(
-                      icon: LineAwesomeIcons.dumbbell_solid,
-                      label: "Gym",
-                      height: 50.0,
-                      redirect: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => InfoGym(),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                  // Container for the button, to match ActivityCard height
-                  SizedBox(
-                    height: 50.0, // Match height with ActivityCard
-                    child: ElevatedButton(
-                      onPressed: () {
-                        print("Add new gym activity");
-                      },
-                      child: const Icon(LineAwesomeIcons.plus_circle_solid),
-                      style: ElevatedButton.styleFrom(
-                        padding:
-                            EdgeInsets.zero, // Remove padding to make it fit
-                        backgroundColor: const Color.fromARGB(255, 138, 105, 82)
-                            .withOpacity(0.5), // Customize button color
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(
-                                10), // No border radius on the right
-                            bottomRight: Radius.circular(
-                                10), // No border radius on the right
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+              Center(
+                child: Row(
+                  children: [
+                    WorkoutTypeButton(icon : 'assets/animals/gym.gif', title: 'GYM', destination: InfoGym()),
+                    WorkoutTypeButton(icon : 'assets/animals/cardio.gif', title: 'CARDIO', destination: InfoGym()),
+                  ],
+                ),
               ),
-              const SizedBox(height: 16),
-              Row(
-                children: [
-                  // Activity card
-                  Expanded(
-                    child: ActivityCard(
-                      icon: LineAwesomeIcons.swimmer_solid,
-                      label: "Swiming",
-                      height: 50.0,
-                      redirect: () {
-                        print("Add new gym activity");
-                      },
-                    ),
-                  ),
-                  // Container for the button, to match ActivityCard height
-                  SizedBox(
-                    height: 50.0, // Match height with ActivityCard
-                    child: ElevatedButton(
-                      onPressed: () {
-                        print("Add new gym activity");
-                      },
-                      style: ElevatedButton.styleFrom(
-                        padding:
-                            EdgeInsets.zero, // Remove padding to make it fit
-                        backgroundColor: const Color.fromARGB(255, 138, 105, 82)
-                            .withOpacity(0.5), // Customize button color
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(
-                                10), // No border radius on the right
-                            bottomRight: Radius.circular(
-                                10), // No border radius on the right
-                          ),
-                        ),
-                      ),
-                      child: const Icon(LineAwesomeIcons.plus_circle_solid),
-                    ),
-                  ),
-                ],
+
+              Center(
+                child: Row(
+                  children: [
+                    WorkoutTypeButton(icon : 'assets/animals/hiking.gif', title: 'HIKING', destination: InfoGym()),
+                    WorkoutTypeButton(icon : 'assets/animals/swimming.gif', title: 'SWIMMING', destination: InfoGym()),
+                  ],
+                ),
               ),
-              const SizedBox(height: 16),
-              Row(
-                children: [
-                  // Activity card
-                  Expanded(
-                    child: ActivityCard(
-                      icon: LineAwesomeIcons.hiking_solid,
-                      label: "Hike",
-                      height: 50.0,
-                      redirect: () {},
-                    ),
-                  ),
-                  // Container for the button, to match ActivityCard height
-                  SizedBox(
-                    height: 50.0, // Match height with ActivityCard
-                    child: ElevatedButton(
-                      onPressed: () {
-                        print("Add new gym activity");
-                      },
-                      style: ElevatedButton.styleFrom(
-                        padding:
-                            EdgeInsets.zero, // Remove padding to make it fit
-                        backgroundColor: const Color.fromARGB(255, 138, 105, 82)
-                            .withOpacity(0.5), // Customize button color
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(
-                                10), // No border radius on the right
-                            bottomRight: Radius.circular(
-                                10), // No border radius on the right
-                          ),
-                        ),
-                      ),
-                      child: const Icon(LineAwesomeIcons.plus_circle_solid),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 16),
-              Row(
-                children: [
-                  // Activity card
-                  Expanded(
-                    child: ActivityCard(
-                      icon: LineAwesomeIcons.running_solid,
-                      label: "Cardio",
-                      height: 50.0,
-                      redirect: () {
-                        print("Add new gym activity");
-                      },
-                    ),
-                  ),
-                  // Container for the button, to match ActivityCard height
-                  SizedBox(
-                    height: 50.0, // Match height with ActivityCard
-                    child: ElevatedButton(
-                      onPressed: () {
-                        print("Add new gym activity");
-                      },
-                      style: ElevatedButton.styleFrom(
-                        padding:
-                            EdgeInsets.zero, // Remove padding to make it fit
-                        backgroundColor: const Color.fromARGB(255, 138, 105, 82)
-                            .withOpacity(0.5), // Customize button color
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(
-                                10), // No border radius on the right
-                            bottomRight: Radius.circular(
-                                10), // No border radius on the right
-                          ),
-                        ),
-                      ),
-                      child: const Icon(LineAwesomeIcons.plus_circle_solid),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context)
-                      .push(MaterialPageRoute(builder: (context) => InfoGym()));
-                },
-                child: const Text('Gym Info'),
-              ),
-              const SizedBox(height: 8),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) =>
-                          const InfoPage(activity_type: "swimming")));
-                },
-                child: const Text('Activity Info'),
+
+              Center(
+                child: Row(
+                  children: [
+                    WorkoutTypeButton(icon : 'assets/animals/gyminfo.gif', title: 'GYM INFO', destination: InfoGym()),
+                    const WorkoutTypeButton(icon : 'assets/animals/activities.gif', title: 'ACTIVITIES', destination: InfoPage(activity_type: "swimming")),
+                  ],
+                ),
               ),
             ],
           ),
@@ -210,3 +69,4 @@ class _WorkoutsState extends State<Workouts> {
     );
   }
 }
+
---
+++ lib/widgets/workout_type_button.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/main.dart';
+
+import '../pages/info_gym.dart';
+
+class WorkoutTypeButton extends StatelessWidget {
+  final String icon;
+  final String title;
+  final Widget destination;
+
+  const WorkoutTypeButton({
+    super.key,
+    required this.icon, // Properly initialize the 'icon' parameter
+    required this.title,
+    required this.destination, // Properly initialize the 'onPressed' parameter
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => destination), // Navigate to destination
+        );
+      },
+      child: Container(
+          width: MediaQuery.of(context).size.width * 0.41,
+          height: 175,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(25),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 2,
+                blurRadius: 5,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+          padding: const EdgeInsets.all(8),
+          child: Column(
+              children: [
+                Image.asset(
+                  icon,
+                ),
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                    color: color,
+                  ),
+                ),
+              ]
+          )
+      ),
+    );
+  }
+}
