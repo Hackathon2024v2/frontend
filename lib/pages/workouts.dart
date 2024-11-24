0f6045b77a7c18cc8f89e39c198884ccb8ea9c31
p1xlized <apaduret@proton.me>
2024-11-24 01:35:25 -0500

    ADD - GynmInfo

--- lib/pages/workouts.dart
+++ lib/pages/workouts.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/objects/User.dart';
-
+import 'package:flutter_application_2/pages/info_gym.dart';
 
 class Workouts extends StatefulWidget {
   const Workouts({super.key});
@@ -12,32 +12,40 @@ class Workouts extends StatefulWidget {
 }
 
 class _WorkoutsState extends State<Workouts> {
-  User userRaw = User(username: "Chill guy", prefix: "Just a ", height: 6.20, score: 900);
-  
+  User userRaw =
+      User(username: "Chill guy", prefix: "Just a ", height: 6.20, score: 900);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        
-        children: [
-          const SizedBox(height: 150,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Expanded(
+        body: Column(
+      children: [
+        const SizedBox(
+          height: 150,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
                 child: Container(
-                  color: Colors.amber,
-                  child: Text('Streak: 4', textAlign: TextAlign.center,),
-                )
+              color: Colors.amber,
+              child: Text(
+                'Streak: 4',
+                textAlign: TextAlign.center,
               ),
-              const SizedBox(height: 10,),
-              
-            ],
-          ),
-
-
-        ],
-      )
-    );
+            )),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) => InfoGym()));
+                },
+                child: Text('gym info'))
+          ],
+        ),
+      ],
+    ));
   }
 }
