1647ae313741e40a80700ad1560f1a2b441cd35e
GiardMathieu <mathieu.giard@hotmail.com>
2024-11-24 02:38:09 -0500

    User class

--- lib/objects/User.dart
+++ lib/objects/User.dart
@@ -1,18 +1,19 @@
-
 class User {
-  final int id;
-  final String username;
-  final String prefix;
-  final double height;
-  final double weight;
-  final int score;
-  final String avatar;
-  final int year;
+  late int id;
+  late String first_name;
+  late String last_name;
+  late String prefix;
+  late double height;
+  late double weight;
+  late int score;
+  late String avatar;
+  late int year;
 
 
   User({
     required this.id,
-    required this.username,
+    required this.first_name,
+    required this.last_name,
     required this.prefix,
     required this.height,
     required this.weight,
@@ -20,4 +21,31 @@ class User {
     required this.avatar,
     required this.year,
   });
+
+  User.fromJson(Map<String, dynamic> json) {
+    id = json['int'];
+    first_name = json['first_name'];
+    last_name = json['last_name'];
+    prefix = json['prefix'];
+    height = json['height'];
+    weight = json['weight'];
+    score = json['score'];
+    avatar = json['avatar'];
+    year = json['year'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['id'] = this.id;
+    data['first_name'] = first_name;
+    data['last_name'] = last_name;
+    data['prefix'] = prefix;
+    data['height'] = height;
+    data['weight'] = weight;
+    data['avatar'] = avatar;
+    data['year'] = year;
+
+    return data;
+  }
+
 }
