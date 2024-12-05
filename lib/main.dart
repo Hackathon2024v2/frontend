b30c1c8089f0c128e22636e66b17a67db56fdf71
Alex  Paduret <72890769+p1xlized@users.noreply.github.com>
2024-12-04 22:53:47 -0500

    Remove Supabase keys

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_2/pages/home_page.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-const supabaseUrl = 'https://xznwmygmnxintzawgghd.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6bndteWdtbnhpbnR6YXdnZ2hkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIzNzY3MjYsImV4cCI6MjA0Nzk1MjcyNn0.51tNyUvk1c5BoMrgguEdfI8_GxzPaVTx1ky3AGB-mcY';
+const supabaseUrl = '';
+const supabaseKey = '';
 const Color color = Color(0xFF8a6952);
 
 void main() async {
